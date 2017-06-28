d4aefbc4a5f913f89b55b313e1c332076bd963c1
desaishivam26 <shivamdesaixda@gmail.com>
2017-06-28 08:13:45 +0000

    Revert "Development: Advanced reboot (2/2)"
    This reverts commit 24672ebaf5e1251050787177de84d1cf249355eb.
    
    Conflicts:
    	res/values/orion_strings.xml

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -217,8 +217,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
 
     private static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
 
-    private static final String ADVANCED_REBOOT = "advanced_reboot";
-
     private static final int RESULT_DEBUG_APP = 1000;
     private static final int RESULT_MOCK_LOCATION_APP = 1001;
 
@@ -246,8 +244,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
     private boolean mDontPokeProperties;
 
     private SwitchPreference mForceAuthorizeSubstratumPackages;
-
-    private SwitchPreference mAdvancedReboot;
 
     private SwitchPreference mEnableAdb;
     private Preference mClearAdbKeys;
@@ -398,10 +394,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         mForceAllowOnExternal = findAndInitSwitchPref(FORCE_ALLOW_ON_EXTERNAL_KEY);
         mPassword = (PreferenceScreen) findPreference(LOCAL_BACKUP_PASSWORD);
         mAllPrefs.add(mPassword);
-
         mForceAuthorizeSubstratumPackages = findAndInitSwitchPref(FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES);
-
-        mAdvancedReboot = findAndInitSwitchPref(ADVANCED_REBOOT);
 
         if (!mUm.isAdminUser()) {
             disableForUser(mEnableAdb);
@@ -409,7 +402,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
             disableForUser(mEnableTerminal);
             disableForUser(mPassword);
             disableForUser(mForceAuthorizeSubstratumPackages);
-	    disableForUser(mAdvancedReboot);
         }
 
         mDebugAppPref = findPreference(DEBUG_APP_KEY);
@@ -763,18 +755,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
     private void updateForceAuthorizeSubstratumPackagesOptions() {
         mForceAuthorizeSubstratumPackages.setChecked(Settings.Secure.getInt(getActivity().getContentResolver(),
                 Settings.Secure.FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES, 0) != 0);
-	updateAdvancedRebootOptions();
-    }
-
-    private void writeAdvancedRebootOptions() {
-        Settings.Secure.putInt(getActivity().getContentResolver(),
-                Settings.Secure.ADVANCED_REBOOT,
-                mAdvancedReboot.isChecked() ? 1 : 0);
-    }
-
-    private void updateAdvancedRebootOptions() {
-        mAdvancedReboot.setChecked(Settings.Secure.getInt(getActivity().getContentResolver(),
-                Settings.Secure.ADVANCED_REBOOT, 0) != 0);
     }
 
     private void resetDangerousOptions() {
@@ -2072,8 +2052,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
             writeForceResizableOptions();
         } else if (preference == mForceAuthorizeSubstratumPackages) {
             writeForceAuthorizeSubstratumPackagesOptions();
-        } else if (preference == mAdvancedReboot) {
-            writeAdvancedRebootOptions();
         } else if (INACTIVE_APPS_KEY.equals(preference.getKey())) {
             startInactiveAppsFragment();
         } else if (BACKGROUND_CHECK_KEY.equals(preference.getKey())) {
